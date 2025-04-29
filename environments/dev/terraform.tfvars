e34fbd7adc1683b32e8c059c59921790b191b90f
Kalpesh Bhangre <31682786+kbrepository@users.noreply.github.com>
2025-04-29 22:11:49 +0530

    Added module to create VPC with subnets.

---
+++ environments/dev/terraform.tfvars
@@ -0,0 +1,4 @@
+vpc_cidr = "20.0.0.0/16"
+public_subnet_cidrs = ["20.0.1.0/24", "20.0.2.0/24"]
+private_subnet_cidrs = ["20.0.3.0/24", "20.0.4.0/24"]
+azs = ["us-east-1a", "us-east-1b"]
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -39,18 +39,18 @@ resource "aws_subnet" "private" {
   }
 }
 
-resource "aws_eip" "nat" {
-  count = var.enable_nat_gateway ? 1 : 0
-  vpc   = true
-}
+# resource "aws_eip" "nat" {
+#   count = var.enable_nat_gateway ? 1 : 0
+# #   vpc   = true
+# # }
 
-resource "aws_nat_gateway" "this" {
-  count         = var.enable_nat_gateway ? 1 : 0
-  allocation_id = aws_eip.nat[0].id
-  subnet_id     = aws_subnet.public[0].id
+# # resource "aws_nat_gateway" "this" {
+# #   count         = var.enable_nat_gateway ? 1 : 0
+# #   allocation_id = aws_eip.nat[0].id
+# #   subnet_id     = aws_subnet.public[0].id
 
-  depends_on = [aws_internet_gateway.this]
-}
+# #   depends_on = [aws_internet_gateway.this]
+# # }
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
@@ -71,22 +71,22 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table" "private" {
-  count  = var.enable_nat_gateway ? 1 : 0
-  vpc_id = aws_vpc.this.id
+# resource "aws_route_table" "private" {
+#   count  = var.enable_nat_gateway ? 1 : 0
+#   vpc_id = aws_vpc.this.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.this[0].id
-  }
+#   route {
+#     cidr_block     = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.this[0].id
+#   }
 
-  tags = {
-    Name = "private-route-table"
-  }
-}
+#   tags = {
+#     Name = "private-route-table"
+#   }
+# }
 
-resource "aws_route_table_association" "private" {
-  count          = var.enable_nat_gateway ? length(aws_subnet.private) : 0
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private[0].id
-}
+# resource "aws_route_table_association" "private" {
+#   count          = var.enable_nat_gateway ? length(aws_subnet.private) : 0
+#   subnet_id      = aws_subnet.private[count.index].id
+#   route_table_id = aws_route_table.private[0].id
+# }

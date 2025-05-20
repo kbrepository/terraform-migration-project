acd55bce4736d5678d9ab293e921c8bebbc1d678
Kalpesh Bhangre <31682786+kbrepository@users.noreply.github.com>
2025-05-20 23:06:29 +0530

    Updated alb module.

---
+++ modules/alb/variables.tf
@@ -0,0 +1,22 @@
+variable "vpc_id" {
+    description = "VPC ID where the ALB will be deployed."
+    type        = string  
+  
+}
+
+variable "subnet_ids" {
+  description = "List of public subnet IDs for the ALB"
+  type        = list(string)
+}
+
+variable "target_group_arns" {
+  description = "List of target group ARNs to attach to the ALB"
+  type        = list(string)
+  default     = []
+}
+
+variable "alb_name" {
+  description = "Name of the Application Load Balancer"
+  type        = string
+  default     = "app-load-balancer"
+}
